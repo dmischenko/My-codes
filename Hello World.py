c2adff212b07b0d6d24068117fb4becb32969e7e
dmischenko <daniel.mischenko@yahoo.com>
2016-08-27 17:36:03 +0300

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,5 @@
+Python 3.6.0a2 (v3.6.0a2:378893423552, Jun 14 2016, 01:01:16) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Hello, World!")
+Hello, World!
+>>> 
